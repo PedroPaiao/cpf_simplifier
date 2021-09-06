e976775dde318f6c5dcedd7374c366ef43855d31
pedro <pedro.silva@pontomais.com.br>
2021-09-06 14:36:29 -0300

    [Setup] gem

--- spec/cpf_simplifier_spec.rb
+++ spec/cpf_simplifier_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe CpfSimplifier do
-  it "has a version number" do
+  it 'has a version number' do
     expect(CpfSimplifier::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'does something useful' do
+    cpf = CpfSimplifier.format_cpf('05398351109')
+    expect(cpf).to eq('053.983.511-09')
   end
 end
