9973643b4e172e85ecd88b7ddf0dc14f8ee2bc04
pedro <pedro.silva@pontomais.com.br>
2021-09-06 14:34:50 -0300

    Initial commit

---
+++ lib/cpf_simplifier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cpf_simplifier/version"
+
+module CpfSimplifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cpf_simplifier_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CpfSimplifier do
+  it "has a version number" do
+    expect(CpfSimplifier::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
